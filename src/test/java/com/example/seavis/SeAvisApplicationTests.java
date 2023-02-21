7e3fefc4eed94f60233782d959765599b1a46707
obijohnkanobi <119489622+obijohnkanobi@users.noreply.github.com>
2023-02-21 10:21:15 +0100

    Initial commit

---
+++ src/test/java/com/example/seavis/SeAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeAvisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
