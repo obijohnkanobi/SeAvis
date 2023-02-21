dd9f832f3c1a99facde429465e090da5bf7aff58
obijohnkanobi <119489622+obijohnkanobi@users.noreply.github.com>
2023-02-21 11:03:01 +0100

    application fil

--- src/main/java/com/example/seavis/SeAvisApplication.java
+++ src/main/java/com/example/seavis/SeAvisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeAvisApplication {
 
+    //spring boot fil
     public static void main(String[] args) {
         SpringApplication.run(SeAvisApplication.class, args);
     }
